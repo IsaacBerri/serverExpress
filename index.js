7f45bf2838e0f1c5e1d71914a1c20a8afd35ea2d
Isaac Berrio <berriisaax18@hotmail.com>
2024-06-26 16:41:12 -0500

    Restringiendo el acceso de los origenes (CORS)

--- index.js
+++ index.js
@@ -6,15 +6,30 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const ACEPTED_ORIGINS = ["https://app-countries2.vercel.app/"];
+
+      if (ACEPTED_ORIGINS.includes(origin)) {
+        return callback(null, true);
+      }
+
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-})
+});
 
 RoutesApp(app);
 connectDB();
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
